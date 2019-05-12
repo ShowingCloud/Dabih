8746c526aa56d8700ce5f74ad8fd95d6eaeacb86
WANG Guoqin @rwgq <wangguoqin1001@gmail.com>
2019-05-12 18:48:15 +0000

    Added Facebook Login and Twitter Login

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 const express = require('express');
 const passport = require('passport');
-const { Strategy } = require('passport-openidconnect');
+const OIDCStrategy = require('passport-openidconnect').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
+
+const config = require('./config');
 
 
-// Configure the Twitter strategy for use by Passport.
-//
-// OAuth 1.0-based strategies require a `verify` function which receives the
-// credentials (`token` and `tokenSecret`) for accessing the Twitter API on the
-// user's behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new Strategy({
+passport.use(new OIDCStrategy({
   clientID: 'sso',
   clientSecret: 'sso_secret',
   authorizationURL: 'https://oidc.scs.im/auth',
   tokenURL: 'https://oidc.scs.im/token',
   callbackURL: 'https://sso.scs.im/callback',
 },
-((token, tokenSecret, profile, cb) => cb(null, profile)
-  // In this example, the user's Twitter profile is supplied as the user
-  // record.  In a production-quality application, the Twitter profile should
-  // be associated with a user record in the application's database, which
-  // allows for account linking and authentication with other identity
-  // providers.
-)));
+(token, tokenSecret, profile, cb) => cb(null, profile)));
 
+passport.use(new FacebookStrategy({
+  clientID: config.facebookAppId,
+  clientSecret: config.facebookAppSecret,
+  callbackURL: 'https://sso.scs.im/auth/facebook/callback',
+},
+(accessToken, refreshToken, profile, done) => done(null, profile)));
 
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
+passport.use(new TwitterStrategy({
+  consumerKey: config.twitterConsumerKey,
+  consumerSecret: config.twitterConsumerSecret,
+  callbackURL: 'https://sso.scs.im/auth/twitter/callback',
+},
+(token, tokenSecret, profile, done) => done(null, profile)));
+
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -44,27 +39,20 @@ passport.deserializeUser((obj, done) => {
 });
 
 
-// Create a new Express application.
 const app = express();
 
-// Configure view engine to render EJS templates.
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({ secret: config.sessionSecret, resave: true, saveUninitialized: true }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-// Define routes.
 app.get('/',
   (req, res) => {
     res.render('home', { user: req.user });
@@ -75,14 +63,32 @@ app.get('/login',
     res.render('login');
   });
 
-app.get('/login/idp',
+app.get('/auth/idp',
   passport.authenticate('openidconnect'));
 
-app.get('/callback',
-  passport.authenticate('openidconnect', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect('/');
-  });
+app.get('/auth/idp/callback',
+  passport.authenticate('openidconnect', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }));
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }));
 
 app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
@@ -90,4 +96,14 @@ app.get('/profile',
     res.render('profile', { user: req.user });
   });
 
-app.listen(3330);
+app.get('/tos',
+  (req, res) => {
+    res.render('tos');
+  });
+
+app.get('/pp',
+  (req, res) => {
+    res.render('pp');
+  });
+
+app.listen(config.port);
