a2b44cf40f9c641fe06ad2cae4d915e8e72e5fa1
WANG Guoqin @rwgq <wangguoqin1001@gmail.com>
2019-06-17 16:38:55 +0000

    Successfully got userinfo

--- providers/meituan.js
+++ providers/meituan.js
@@ -25,23 +25,26 @@ module.exports = (list) => {
   });
 
   strategy.userProfile = (accessToken, done) => {
-    console.log(accessToken);
-    strategy._oauth2.get(`https://openapi.waimai.meituan.com/oauth/userinfo?app_id=${config.meituanAppId}&secret=${config.meituanAppSecret}`,
-      accessToken, (err, body) => {
-        try {
-          const json = JSON.parse(body);
+    strategy._oauth2._request('POST',
+      `https://openapi.waimai.meituan.com/oauth/userinfo?app_id=${config.meituanAppId}&secret=${config.meituanAppSecret}&access_token=${accessToken}`,
+      null, null, null, (err, body) => {
+        if (err) done(err);
+        else {
+          try {
+            const json = JSON.parse(body);
 
-          const profile = { provider: 'meituan' };
-          profile.id = json.openid;
-          profile.nickname = json.nicknamee;
-          profile.avatar = json.avatar;
-          profile.phone = json.desensitization_phone;
-          profile._raw = body;
-          profile._json = json;
+            const profile = { provider: 'meituan' };
+            profile.id = json.openid;
+            profile.nickname = json.nickname;
+            profile.avatar = json.avatar;
+            profile.phone = json.desensitization_phone;
+            profile._raw = body;
+            profile._json = json;
 
-          done(null, profile);
-        } catch (error) {
-          done(error);
+            done(null, profile);
+          } catch (error) {
+            done(error);
+          }
         }
       });
   };
