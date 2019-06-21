d8c6502305575aed531d65c4e3a53d16a9bb2bcc
WANG Guoqin @rwgq <wangguoqin1001@gmail.com>
2019-06-21 03:33:37 +0000

    Filtering required login provider

--- routes/common.js
+++ routes/common.js
@@ -5,28 +5,31 @@ const config = require('../config/config');
 
 module.exports = (app) => {
   app.get('/',
-    (req, res) => {
-      res.render('home', { user: req.user });
-    });
+    (req, res) => res.render('home', { user: req.user }));
 
   app.get('/login',
+    (req, res) => res.render('login', { providers: config.providers }));
+
+  app.get('/login/:provider',
+    (req, res) => {
+      res.render('login', {
+        providers: config.providers.filter(provider => provider.provider === req.params.provider)
+          || config.providers,
+      });
+    });
+
+  app.get('/login/required/:required',
     (req, res) => {
       res.render('login', { providers: config.providers });
     });
 
   app.get('/profile',
     ensureLogin.ensureLoggedIn(),
-    (req, res) => {
-      res.render('profile', { user: req.user });
-    });
+    (req, res) => res.render('profile', { user: req.user }));
 
   app.get('/tos',
-    (req, res) => {
-      res.render('tos');
-    });
+    (req, res) => res.render('tos'));
 
   app.get('/pp',
-    (req, res) => {
-      res.render('pp');
-    });
+    (req, res) => res.render('pp'));
 };
